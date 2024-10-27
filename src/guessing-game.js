a8354d3a5e3474c8b6a527c87373682239d75d8e
tmkvch18 <tmkvch18@gmail.com>
2024-10-27 16:39:55 +0300

    feat: add solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,25 @@
 class GuessingGame {
-    constructor() {}
+  constructor() {}
 
-    setRange(min, max) {
+  setRange(min, max) {
+    this.min = min;
+    this.max = max;
+  }
 
-    }
+  guess() {
+    // console.log(this)
+    return (this.mid = Math.round((this.max + this.min) / 2));
+  }
 
-    guess() {
+  lower() {
+    // console.log(this)
+    return (this.max = this.mid);
+  }
 
-    }
-
-    lower() {
-
-    }
-
-    greater() {
-
-    }
+  greater() {
+    // console.log(this)
+    return (this.min = this.mid);
+  }
 }
 
 module.exports = GuessingGame;
